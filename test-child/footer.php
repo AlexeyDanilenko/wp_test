10f226814bc9799e216e12f2b24224738e45b850
Alexey Danilenko <38225162+AlexeyDanilenko@users.noreply.github.com>
2018-04-11 19:50:52 +0300

    Add files via upload

---
+++ test-child/footer.php
@@ -0,0 +1,11 @@
+			<div style="clear: both;"></div>
+
+			<div class="footer">
+				<div class="container">
+					<a href="#"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt=""></a>
+				</div>
+			</div><!-- /.footer -->
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
