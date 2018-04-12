1ae7318fd6a1c953620039f9d1206a53c062b9a4
Alexey Danilenko <38225162+AlexeyDanilenko@users.noreply.github.com>
2018-04-12 17:38:55 +0300

    Add files via upload

--- test/header.php
+++ test/header.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="description" content="">
-		<title>WP-Test index page</title>
+		<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
 
 		<?php wp_head(); ?>
 
